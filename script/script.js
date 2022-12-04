75ca3a15d5380cf4a008010bf4a2c817715f172d
Daryna <condordaryna@gmail.com>
2022-12-05 00:52:21 +0200

    make changes to the js file

--- script/script.js
+++ script/script.js
@@ -3,7 +3,16 @@ button = document.getElementById("btn");
 errorMessage = document.querySelector(".main__message");
 
 telephone.oninput = function () {
-    telephone.value.length >= 12 ? button.classList.add("press") : button.classList.remove("press");
+    telephone.value.length == 12 ? button.classList.add("press") : button.classList.remove("press");
+    if (telephone.value == "+38000000000") {
+        errorMessage.classList.remove("hiden");
+        telephone.classList.add("error");
+        button.classList.remove("press");
+    }
+    else if (telephone.value.length < 12) {
+        errorMessage.classList.add("hiden");
+        telephone.classList.remove("error");
+    }
 };
 
 button.addEventListener("click", () => {
@@ -11,10 +20,6 @@ button.addEventListener("click", () => {
         window.open("pin.html");
         errorMessage.classList.add("hiden");
         telephone.classList.remove("error");
-        button.classList.remove("press");
-    } else if (telephone.value == "+38000000000") {
-        errorMessage.classList.remove("hiden");
-        telephone.classList.add("error");
         button.classList.remove("press");
     }
 });
